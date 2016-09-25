efef4c4c7eda1eb673e31251fd101c6574af6483
井上航 <k.prfm19@gmail.com>
2016-09-26 02:52:48 +0900

    ImageRecognition.frameworkを無事導入。これからpostrequest

---
+++ ImageRecogination/ImageRecogination-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ImageRecogination-Bridging-Header.h
+//  ImageRecogination
+//
+//  Created by 井上航 on 2016/09/26.
+//  Copyright © 2016年 Wataru Inoue. All rights reserved.
+//
+
+#ifndef ImageRecogination_Bridging_Header_h
+#define ImageRecogination_Bridging_Header_h
+#import <ImageRecognition/ImageRecognition.h>
+
+#endif /* ImageRecogination_Bridging_Header_h */
--- ImageRecogination/ViewController.swift
+++ ImageRecogination/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import ImageRecognition
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(animated: Bool) {
+        if let theImage = imageView.image {
+            search(theImage)
+        }
+    }
+    
+    @IBAction func onTappedAlbumButton(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .PhotoLibrary
+            picker.delegate = self
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    // 写真が選択された時に呼び出されるメソッド
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
+        imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    func search(image: UIImage) {
+        
+//        let recognize = ImageRecognition()
+        let requestUrl = "https://api.apigw.smt.docomo.ne.jp/imageRecognition/v1/concept/classify/"
+        let requestParams = RequestParams()
+        requestParams.apiKey = "4c69522e6c6f616e52467936476d2f384b7233583452556369764466716f422e4c676b51646e38654b5835"
+        requestParams.recognitionType = "flower"
+        requestParams.contentType = "multipart/form-data"
+        requestParams.image = image
+        
+//        do {
+//            let resultData = try recognize.recognize(requestParams)
+//            print(resultData)
+//        } catch {
+//            print("error: xcodeの問題")
+//        }
+    }
 }
 
